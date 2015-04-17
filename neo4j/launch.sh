e96aa188b9072a333e4311d381a3a7216949161e
zyh226 <zyh226@yeah.net>
2015-04-17 00:17:02 -0700

    edit launch.sh

--- neo4j/launch.sh
+++ neo4j/launch.sh
@@ -2,8 +2,6 @@
 
 NEO4J_HOME=/var/lib/neo4j
 
-sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=$HOSTNAME|g" $NEO4J_HOME/conf/neo4j-server.properties
-
 # doing this conditionally in case there is already a limit higher than what
 # we're setting here. neo4j recommends at least 40000.
 # 
